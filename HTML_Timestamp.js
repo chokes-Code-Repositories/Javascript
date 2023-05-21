37974fd9c0ffee421195733226094dec56a89535
choke <47597850+choke-dev@users.noreply.github.com>
2023-05-22 00:27:55 +0800

    Update HTML_Timestamp.js

--- HTML_Timestamp.js
+++ HTML_Timestamp.js
@@ -7,133 +7,103 @@ t:UNIX_TIMESTAMP:d = [Month]/[Day]/[Year]
 t:UNIX_TIMESTAMP:T = [Hour]:[Minute]:[Second] [AM/PM]
 t:UNIX_TIMESTAMP:t = [Hour]:[Minute] [AM/PM]
 */
+const htmlElement = document.querySelector('html');
+const preferredLanguage = htmlElement.getAttribute('lang') || navigator.language;
 function formatTimestamp(text) {
     const regex = /t:(\d+):([RFDTfdt])/g;
     const now = Math.floor(Date.now() / 1000).toFixed(0);
     return text.replace(regex, (match, timestamp, format) => {
         const unixTimestamp = parseInt(timestamp, 10);
-        const validTimestamp = (new Date(unixTimestamp)).getTime() > 0;
-        if (!validTimestamp) {
-            return "Invalid unix timestamp (Invalid date)";
+        if (isNaN(unixTimestamp)) {
+          return match; // Invalid timestamp, return the original match
         }
         const timeDiff = unixTimestamp - now;
         switch (format) {
-            case 'R':
-                return getTimeAgoString(timeDiff);
-            case 'F':
-                return formatDateWithTime(unixTimestamp, true, true);
-            case 'f':
-                return formatDateWithTime(unixTimestamp, false, true);
-            case 'D':
-                return formatDateWithTime(unixTimestamp, false, false);
-            case 'd':
-                return formatDateSlashSeparated(unixTimestamp);
-            case 'T':
-                return formatTime(unixTimestamp, true);
-            case 't':
-                return formatTime(unixTimestamp, false);
-            default:
-                return "Invalid unix timestamp (Invalid format)";
+          case 'R':
+            return getTimeAgoString(timeDiff);
+          case 'F':
+            return formatDateWithTime(unixTimestamp, true, true);
+          case 'f':
+            return formatDateWithTime(unixTimestamp, false, true);
+          case 'D':
+            return formatDate(unixTimestamp);
+          case 'd':
+            return formatDateSlashSeparated(unixTimestamp);
+          case 'T':
+            return formatTime(unixTimestamp, true);
+          case 't':
+            return formatTime(unixTimestamp, false);
+          default:
+            return match; // Unknown format, return the original match
         }
     });
 }
 
 function getTimeAgoString(seconds) {
     const absSeconds = Math.abs(seconds);
-    const isPast = seconds < 0;
+    const rtf = new Intl.RelativeTimeFormat(preferredLanguage, { numeric: 'auto' });
     if (absSeconds < 60) {
-        const isSingular = (Math.floor(absSeconds) === 1);
-        return isPast ? `${absSeconds} second${isSingular ? '' : 's'} ago` : `in ${absSeconds} second${isSingular ? '' : 's'}`;
+        return rtf.format(seconds, 'second');
+    } else if (absSeconds < 3600) {
+        const minutes = Math.floor(seconds / 60);
+        return rtf.format(minutes, 'minute');
+    } else if (absSeconds < 86400) {
+        const hours = Math.floor(seconds / 3600);
+        return rtf.format(hours, 'hour');
+    } else if (absSeconds < 604800) {
+        const days = Math.floor(seconds / 86400);
+        return rtf.format(days, 'day');
+    } else if (absSeconds < 2628000) {
+        const weeks = Math.floor(seconds / 604800);
+        return rtf.format(weeks, 'week');
+    } else if (absSeconds < 31536000) {
+        const months = Math.floor(seconds / 2628000);
+        return rtf.format(months, 'month');
+    } else {
+        const years = Math.floor(seconds / 31536000);
+        return rtf.format(years, 'year');
     }
-    if (absSeconds < 3600) {
-        const minutes = Math.floor(absSeconds / 60);
-        const isSingular = (Math.floor(minutes) === 1);
-        return isPast ? `${minutes} minute${isSingular ? '' : 's'} ago` : `in ${minutes} minute${isSingular ? '' : 's'}`;
-    }
-    if (absSeconds < 86400) {
-        const hours = Math.floor(absSeconds / 3600);
-        const isSingular = (Math.floor(hours) === 1);
-        return isPast ? `${hours} hour${isSingular ? '' : 's'} ago` : `in ${hours} hour${isSingular ? '' : 's'}`;
-    }
-    if (absSeconds < 604800) {
-        const days = Math.floor(absSeconds / 86400);
-        const isSingular = (Math.floor(days) === 1);
-        return isPast ? `${days} day${isSingular ? '' : 's'} ago` : `in ${days} day${isSingular ? '' : 's'}`;
-    }
-    if (absSeconds < 2628000) {
-        const weeks = Math.floor(absSeconds / 604800);
-        const isSingular = (Math.floor(weeks) === 1);
-        return isPast ? `${weeks} week${isSingular ? '' : 's'} ago` : `in ${weeks} week${isSingular ? '' : 's'}`;
-    }
-    if (absSeconds < 31536000) {
-        const months = Math.floor(absSeconds / 2628000);
-        const isSingular = (Math.floor(months) === 1);
-        return isPast ? `${months} month${isSingular ? '' : 's'} ago` : `in ${months} month${isSingular ? '' : 's'}`;
-    }
-    const years = Math.floor(absSeconds / 31536000);
-    const isSingular = (Math.floor(years) === 1);
-    return isPast ? `${years} year${isSingular ? '' : 's'} ago` : `in ${years} year${isSingular ? '' : 's'}`;
 }
 
 function formatDateWithTime(unixTimestamp, includeDayOfWeek, includeYear) {
+    // use Intl.DateTimeFormat
     const date = new Date(unixTimestamp * 1000);
-    const dayOfWeek = getDayOfWeekString(date.getDay());
-    const month = getMonthString(date.getMonth());
-    const day = date.getDate();
-    const year = date.getFullYear();
-    const time = formatTime(unixTimestamp, true);
-    let formattedDate = '';
-    if (includeDayOfWeek) {
-        formattedDate += dayOfWeek + ', ';
-    }
-    formattedDate += month + ' ' + day;
-    if (includeYear) {
-        formattedDate += ', ' + year;
-    }
-    formattedDate += ' at ' + time;
-    return formattedDate;
+    const options = {
+        weekday: includeDayOfWeek ? 'long' : undefined,
+        year: includeYear ? 'numeric' : undefined,
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    };
+    return new Intl.DateTimeFormat(preferredLanguage, options).format(date);
 }
-
+function formatDate(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000);
+    const options = {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    };
+    return new Intl.DateTimeFormat(preferredLanguage, options).format(date);
+}
 function formatDateSlashSeparated(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000);
-    const month = padNumber(date.getMonth() + 1);
-    const day = padNumber(date.getDate());
-    const year = date.getFullYear();
-    return `${month}/${day}/${year}`;
+    const options = {
+        month: 'numeric',
+        day: 'numeric',
+        year: 'numeric'
+    };
+    return new Intl.DateTimeFormat(preferredLanguage, options).format(date);
 }
-
 function formatTime(unixTimestamp, includeSeconds) {
     const date = new Date(unixTimestamp * 1000);
-    let hours = date.getHours();
-    const minutes = padNumber(date.getMinutes());
-    const seconds = includeSeconds ? ':' + padNumber(date.getSeconds()) : '';
-    const period = hours >= 12 ? 'PM' : 'AM';
-    if (hours === 0) {
-        hours = 12;
-    } else if (hours > 12) {
-        hours -= 12;
-    }
-    return `${hours}:${minutes}${seconds} ${period}`;
+    const options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: includeSeconds ? 'numeric' : undefined,
+        hour12: true
+    };
+    return new Intl.DateTimeFormat(preferredLanguage, options).format(date);
 }
-
-function getDayOfWeekString(day) {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return daysOfWeek[day];
-}
-
-function getMonthString(month) {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    return months[month];
-}
-
-function padNumber(number) {
-    return number.toString().padStart(2, '0');
-}
-
-console.log(formatTimestamp("t:1684370543:R"));
-console.log(formatTimestamp("t:1684370543:F"));
-console.log(formatTimestamp("t:1684370543:f"));
-console.log(formatTimestamp("t:1684370543:D"));
-console.log(formatTimestamp("t:1684370543:d"));
-console.log(formatTimestamp("t:1684370543:T"));
-console.log(formatTimestamp("t:1684370543:t"));
