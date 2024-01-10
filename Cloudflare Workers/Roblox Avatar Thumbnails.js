e29b72d3b41c67a937f848e967558d93eb6bd795
choke <47597850+choke-dev@users.noreply.github.com>
2024-01-10 22:00:38 +0800

    Update Roblox Avatar Thumbnails.js

--- Cloudflare Workers/Roblox Avatar Thumbnails.js
+++ Cloudflare Workers/Roblox Avatar Thumbnails.js
@@ -1,127 +1,99 @@
-const validSizes = [
-  '30x30',
-  '48x48',
-  '60x60',
-  '75x75',
-  '100x100',
-  '110x110',
-  '140x140',
-  '150x150',
-  '150x200',
-  '180x180',
-  '250x250',
-  '352x352',
-  '420x420',
-  '720x720'
+// src/worker.js
+var cacheDuration = 300 * 1e3;
+var expirationTime = Date.now() + cacheDuration;
+var apiUrl = "https://thumbnails.roblox.com";
+var validSizes = [
+  "30x30",
+  "48x48",
+  "60x60",
+  "75x75",
+  "100x100",
+  "110x110",
+  "140x140",
+  "150x150",
+  "150x200",
+  "180x180",
+  "250x250",
+  "352x352",
+  "420x420",
+  "720x720"
 ];
-
-// Create a cache object to store previously fetched images
-const imageCache = new Map();
-
-export default {
+var imageCache = /* @__PURE__ */ new Map();
+var worker_default = {
   async fetch(request, env, ctx) {
-    // Extract the URI path, user ID, and size parameter from the request
     const url = new URL(request.url);
-    const path = url.pathname.replace('/roblox/avatar-thumbnails', ''); // Remove the prefix
-    const userId = url.searchParams.get('userId'); // Get the user ID from query parameter
-    const size = url.searchParams.get('size'); // Get the size parameter from query parameter
-    
-    // Check if the userId parameter is present
+    const path = url.pathname.replace("/roblox/avatar-thumbnails", "");
+    const userId = url.searchParams.get("userId");
+    const size = url.searchParams.get("size");
     if (!userId) {
       return new Response("Missing required userId parameter", { status: 400 });
     }
-
-    // Check if the userId parameter is an integer
     if (!Number(userId)) {
-      return new Response("userId is not an Integer.");
+      return new Response("userId is not an Integer.", { status: 400 });
     }
-
-    // If the size parameter is defined, check if it is a valid size.
-    if (size) {
-      if (!validSizes.includes(size)) {
-        return new Response("Invalid size parameter.");
-      }
+    if (size && !validSizes.includes(size)) {
+      return new Response("Invalid size parameter.", { status: 400 });
     }
-
-    // Define the base URL for the API
-    const apiUrl = "https://thumbnails.roblox.com";
-
-    // Check if the image is already in the cache
-    const cacheKey = `${userId}-${size || 'default'}`;
-    if (imageCache.has(cacheKey)) {
-
-      const { imageResponse, expirationTime } = imageCache.get(cacheKey);
-      if (expirationTime > Date.now()) {
-        console.log(`Found cached image data for UserID: ${userId} with size ${size}, Returning cached image...`)
-        return new Response(imageResponse, {
-          headers: {
-            "Content-Type": "image/png", // Adjust the content type if needed
-          },
-        });
-      } else {
-        // Remove expired cache entry
-        imageCache.delete(cacheKey);
-      }
-
-    }
-
-    // Determine the size to use (either from parameter or default)
     let imageSize;
     if (path === "/v1/users/avatar-bust") {
-      // Use the specified size if provided, or default to 420x420
       imageSize = size || "420x420";
-    
-      // Cap the size at 420x420 if it's larger
       if (validSizes.indexOf(imageSize) === -1 || validSizes.indexOf(imageSize) > validSizes.indexOf("420x420")) {
-        console.warn("Client tried to request a size larger than 420x420 on the /v1/users/avatar-bust endpoint, Overriden to 420x420.")
+        console.warn(`Client tried to request a size larger than 420x420 for ${path}, Overridden to 420x420.`);
         imageSize = "420x420";
       }
+      if (validSizes.indexOf(imageSize) < validSizes.indexOf("48x48")) {
+        console.warn(`Client tried to request a size smaller than 48x48 for ${path}, Overridden to 48x48.`);
+        imageSize = "48x48";
+      }
+    } else if (path === "/v1/users/avatar-headshot") {
+      imageSize = size || "720x720";
+      if (validSizes.indexOf(imageSize) < validSizes.indexOf("48x48")) {
+        console.warn(`Client tried to request a size smaller than 48x48 for ${path}, Overridden to 48x48.`);
+        imageSize = "48x48";
+      }
     } else {
-      // Use the specified size if provided, or default to 720x720
       imageSize = size || "720x720";
     }
-
-    // Fetch data from the corresponding API endpoint
-    const response = await fetch(`${apiUrl}${path}?userIds=${userId}&size=${imageSize}&format=Png&isCircular=false`);
-
-    // Check if the response is successful
+    const cacheKey = `${userId}-${path}-${imageSize}`;
+    console.log(`Cachekey: ${userId}-${path}-${imageSize}`);
+    if (imageCache.has(cacheKey)) {
+      const { imageResponse, expirationTime: expirationTime2 } = imageCache.get(cacheKey);
+      if (expirationTime2 > Date.now()) {
+        console.log(`Found cached image data for UserID: ${userId} in ${path} with size ${imageSize}, Returning cached image...`);
+        return new Response(null, {
+          status: 302,
+          headers: {
+            "Location": imageResponse
+          }
+        });
+      } else {
+        imageCache.delete(cacheKey);
+      }
+    }
+    const response = await fetch(`${apiUrl}${path}?userIds=${userId}&size=${imageSize}&format=Png&isCircular=false`).catch((err) => {
+      console.log(`[ERROR] Fetch request failed.`);
+    });
     if (!response.ok) {
       return new Response("Failed to fetch data from the URL", { status: response.status });
     }
-
-    // Parse the JSON response
     const responseData = await response.json();
-
-    // Extract the imageUrl from the JSON data
-    const imageUrl = responseData.data[0].imageUrl;
-
-    // Fetch the image data
-    const imageResponse = await fetch(imageUrl);
-
-    // Check if the image response is successful
-    if (!imageResponse.ok) {
-      return new Response("Failed to fetch image data", { status: imageResponse.status });
+    const imageUrl = responseData?.data[0].imageUrl;
+    if (!imageUrl) {
+      return new Response(`Roblox API did not return an image.`);
     }
-
-    // Get the image data as a Uint8Array
-    const imageBuffer = await imageResponse.arrayBuffer();
-
-    // Create a response with the image data and appropriate headers
-    const imageResponseCached = new Response(imageBuffer, {
-      headers: {
-        "Content-Type": "image/png", // Adjust the content type if needed
-      },
-    });
-
-    // Store the image data in the cache with an expiration
-    const cacheDuration = 300 * 1000; // 5 minutes
-    const expirationTime = Date.now() + cacheDuration;
-
     imageCache.set(cacheKey, {
-      imageResponse: imageBuffer, 
+      imageResponse: imageUrl,
       expirationTime
     });
-
-    return imageResponseCached;
-  },
+    return new Response(null, {
+      status: 302,
+      headers: {
+        "Location": imageUrl
+      }
+    });
+  }
 };
+export {
+  worker_default as default
+};
+//# sourceMappingURL=worker.js.map
